ede6942019b6028ba403ed78abab25a489b0b76f
tvessiere <vessiere.thomas@hotmail.com>
2017-02-23 17:15:07 +0100

    not test

--- add_software.py
+++ add_software.py
@@ -1,5 +1,5 @@
 __author__ = "Vessiere Thomas <vessiere.thomas@hotmail.com>"
-__Copyright__ = "Copyright 2010-2017 University of Liège, Belgium, http://www.cytomine.be/"
+__Copyright__ = "Copyright 2010-2017 University of Liege, Belgium, http://www.cytomine.be/"
 
 
 import os
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     cytomine_host = "demo.cytomine.be"
     Pk = 'cbfe0e04-3fd7-4a7f-a13c-b86685ecb570'
-    Prk = 'XXXXXXX'
+    Prk = 'XXXXX'
     id_project = 19941904
 
     # Connection to Cytomine Core
@@ -26,31 +26,32 @@ if __name__ == "__main__":
     )
 
     #add software with execute command
-    software = conn.add_software("SE_TranslationAffine2","createRabbitJobWithArgsService","download","python algo/Simple_Elastix/WorkFlowSE.py"+
-                                                                                                     "--cytomine_host $host" +
-                                                                                                     "--cytomine_public_key $publicKey" +
-                                                                                                     "--cytomine_private_key $privateKey" +
-                                                                                                     "--cytomine_base_path /api/" +
+    software = conn.add_software("SE_TranslationAffine4","createRabbitJobWithArgsService","download","python algo/Simple_Elastix/WorkFlowSE.py"+
+                                                                                                     "--cytomine_host $cytomine_host" +
+                                                                                                     "--cytomine_public_key $cytomine_public_key" +
+                                                                                                     "--cytomine_private_key $cytomine_private_key" +
                                                                                                      "--cytomine_id_software $cytomine_id_software" +
-                                                                                                     "--cytomine_working_path algo/Simple_Elastix/images" +
+                                                                                                     "--cytomine_working_path algo/simple_elastix" +
                                                                                                      "--cytomine_id_project $cytomine_id_project" +
-                                                                                                     "--id_fix_image $id_fix_image" +
-                                                                                                     "--id_mov_image $id_mov_image" +
+                                                                                                     "--fix_image_id fix_image" +
+                                                                                                     "--mov_image_id $id_mov_image" +
                                                                                                      "--nb_iterations $nb_iterations" +
-                                                                                                     "--nb_spatialsampels $nb_spatialsampels")
-
-    #conn.add_software_parameter(name="cytomine_id_software",id_software=software.id,type = "Number",default_value=None,required=True,index=1,set_by_server=True)
-    #conn.add_software_parameter(name="cytomine_id_project",id_software=software.id,type= "Number",default_value=None,required=True,index=10,set_by_server=True)
-
-    conn.add_software_parameter(name="id_fix_image",id_software=software.id,type= "Number",default_value=None,required=True,index=20,set_by_server=False)
-    conn.add_software_parameter(name="id_mov_image",id_software=software.id,type= "Number",default_value=None,required=True,index=30,set_by_server=False)
-    conn.add_software_parameter(name="nb_iterations",id_software=software.id,type= "Number",default_value=None,required=True,index=40,set_by_server=False)
-    conn.add_software_parameter(name="nb_spatialsampels",id_software=software.id,type= "Number",default_value=None,required=True,index=50,set_by_server=False)
-
-    """conn.delete_software_parameter(19957884)
-    conn.delete_software_parameter(19957890)
-    conn.delete_software_parameter(19957896)
-    conn.delete_software_parameter(19957902)"""
+                                                                                                     "--nb_spatialsampels $nb_spatial_sample" +
+                                                                                                     "--cytomine_storage_id $storage" +
+                                                                                                     "--cytomine_annotation_fix_id $annotation_fix_id" +
+                                                                                                     "--cytomine_annotation_moving_id $annotation_moving_id" +
+                                                                                                     "--cytomine_upload demo-upload.cytomine.be"
+                                 )
 
 
-
+    conn.add_software_parameter(name="fix_image",id_software=software.id,type= "Number",default_value=None,required=True,index=10,set_by_server=False)
+    conn.add_software_parameter(name="moving_image",id_software=software.id,type= "Number",default_value=None,required=True,index=20,set_by_server=False)
+    conn.add_software_parameter(name="nb_iterations",id_software=software.id,type= "Number",default_value=None,required=True,index=30,set_by_server=False)
+    conn.add_software_parameter(name="nb_spatial_sample",id_software=software.id,type= "Number",default_value=None,required=True,index=40,set_by_server=False)
+    conn.add_software_parameter(name="storage",id_software=software.id,type= "Number",default_value=None,required=True,index=50,set_by_server=False)
+    conn.add_software_parameter(name="annotation_fix_id",id_software=software.id,type= "Number",default_value=None,required=False,index=60,set_by_server=False)
+    conn.add_software_parameter(name="annotation_moving_id",id_software=software.id,type= "Number",default_value=None,required=False,index=70,set_by_server=False)
+    conn.add_software_parameter(name="upload_server",id_software=software.id,type= "Number",default_value=None,required=True,index=80,set_by_server=True)
+    conn.add_software_parameter(name="annotation_moving_id",id_software=software.id,type= "Number",default_value=None,required=False,index=90,set_by_server=True)
+    conn.add_software_parameter(name="cytomine_id_software",id_software=software.id,type= "Number",default_value= 0,required= True, index=100, set_by_server=True)
+    conn.add_software_parameter(name="cytomine_id_project",id_software=software.id,type= "Number",default_value= 0,required= True, index=110, set_by_server=True)
