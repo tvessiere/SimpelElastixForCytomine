2f22c75235ea8acd8a715ece031b933312af3ae5
tvessiere <vessiere.thomas@hotmail.com>
2017-02-23 17:15:40 +0100

    remove files

--- get_and_move.py
+++ get_and_move.py
@@ -1,30 +1,32 @@
-import os
-from argparse import ArgumentParser
 
-from cytomine.models import annotation
+__author__ = "Vessiere Thomas <vessiere.thomas@hotmail.com>"
+__Copyright__ = "Copyright 2010-2017 University of Liège, Belgium, http://www.cytomine.be/"
+
+from argparse import ArgumentParser
 from sldc import Loggable, Logger, StandardOutputLogger
 from cytomine import Cytomine
 from cytomine_utilities import CytomineJob
 import SimpleITK as sitk
 import scipy.misc as misc
 import numpy as np
+import shutil
 import os
 
 class SimpleElastixJob(CytomineJob, Loggable):
 
     # set parameters for your algo, 4 first params must be like this #
     def __init__(self, cytomine, software_id, project_id, job_parameters,
-                 fix_image_id, moving_image_id, nb_spatial_sampel, nb_iterations, storage_id,
+                 fix_image_id, moving_image_id, nb_spatial_sample, nb_iterations, storage_id,
                  id_annotation_fix, id_annotation_moving, working_path, cytomine_host, cytomine_upload, pk, prk):
 
         # call init from parent classes #
-        CytomineJob.__init__(self, cytomine, software_id, project_id, parameters=job_parameters)
+        CytomineJob.__init__(self, cytomine, software_id, project_id, """parameters=job_parameters""" )
         Loggable.__init__(self, logger=StandardOutputLogger(Logger.INFO))
 
         # keep var from parameters #
         self._fix_image_id = fix_image_id
         self._moving_image_id = moving_image_id
-        self._nb_spatial_sampel = nb_spatial_sampel
+        self._nb_spatial_sampel = nb_spatial_sample
         self._nb_iterations = nb_iterations
         self._storage_id = storage_id
         self._id_annotation_fix = id_annotation_fix
@@ -43,45 +45,15 @@ class SimpleElastixJob(CytomineJob, Loggable):
 
         if not self._id_annotation_fix and not self._id_annotation_fix:
             # dump images #
-            self._cytomine.dump_project_images(id_project=self._project_id, dest_path="/images/" , override=True, max_size=True)
+            self._cytomine.dump_project_images(id_project=self._project_id, dest_path=os.path.join(self._working_path, "images" , str(self.job.id)), override=True, max_size=True)
 
-            # read images #
-            fix_image = sitk.ReadImage(self._working_path + "/images/" + str(self._project_id) + "/" + str(self._fix_image_id) + ".jpg",sitk.sitkFloat32)
-            mov_image = sitk.ReadImage(self._working_path + "/images/" + str(self._project_id) + "/" + str(self._fix_image_id) + ".jpg",sitk.sitkFloat32)
+            # format paths #
+            path_to_fix_image = os.path.join(self._working_path,"images",str(self.job.id),str(self._fix_image_id))
+            path_to_moving_image = os.path.join(self._working_path, "images", str(self.job.id), str(self._moving_image_id))
 
-            # instanciate and processing with SimpleElastix #
-            simple_elastix = sitk.SimpleElastix()
-            simple_elastix.SetFixedImage(fix_image)
-            simple_elastix.SetMovingImage(mov_image)
-            parameterMapTranslation = sitk.GetDefaultParameterMap("translation")
-            parameterMapAffine = sitk.GetDefaultParameterMap("affine")
-
-            # 2 params, Translation and affine #
-            simple_elastix.SetParameterMap(parameterMapTranslation)
-            simple_elastix.AddParameterMap(parameterMapAffine)
-
-            # params set by user #
-            simple_elastix.SetParameter("MaximumNumberOfIterations", str(self._nb_iterations))
-            simple_elastix.SetParameter("NumberOfSpatialSamples", str(self._nb_spatialsampels))
-
-            # run processing #
-            simple_elastix.Execute()
-
-            # get result image and fix image for upload #
-            img = simple_elastix.GetResultImage()
-            np_img = sitk.GetArrayFromImage(img)
-            img_to_save = np.zeros((np_img.shape[0], np_img.shape[1], 3))
-            img_to_save[:, :, 1] = np_img
-            img_to_save[:, :, 2] = sitk.GetArrayFromImage(fix_image)
-            misc.imsave(self._working_path + "/images/" + "result_translationaffine.png" , img_to_save)
-
-            # upload config #
-            uploadConn = Cytomine(self._cytomine_upload, self._pk, self._prk, verbose=False)
-            sync = False
-
-            # uploading image #
-            uploadConn.upload_image(self._working_path + "/images/" +"result_translationaffine.png",
-                                    self._project_id, self._storage_id, "http://" + self._cytomine_host)
+            # debug #
+            print "path_to_fix_image : " + str(path_to_fix_image)
+            print "path_to_moving_image" + str(path_to_moving_image)
 
         else:
             # dump annotations#
@@ -93,7 +65,7 @@ class SimpleElastixJob(CytomineJob, Loggable):
                                     (
                                         annotations=collection_fix,
                                         get_image_url_func=self._cytomine.Annotation.get_annotation_crop_url,
-                                        dest_path=self._working_path + "/image_fix/",
+                                        dest_path= os.path.join(self._working_path,"images",str(self.job.id),"annotation_fix"),
                                         desired_zoom=0
                                     )
 
@@ -105,7 +77,7 @@ class SimpleElastixJob(CytomineJob, Loggable):
                                     (
                                         annotations=collection_moving,
                                         get_image_url_func=self._cytomine.Annotation.get_annotation_crop_url,
-                                        dest_path=self._working_path + "/image_mov/",
+                                        dest_path = os.path.join(self._working_path,"images",str(self.job.id),"annotation_moving"),
                                         desired_zoom=0
                                     )
 
@@ -113,46 +85,54 @@ class SimpleElastixJob(CytomineJob, Loggable):
             id_term = annotation_fix.term[0]
 
             # because the name of the file is vague, just list the file and get the elem at 0 #
-            list_fix = os.listdir(self._working_path + "/image_fix/" + str(id_term) + "/")
-            list_moving = os.listdir(self._working_path + "/imag_mov/" + str(id_term) + "/")
+            list_fix = os.listdir(os.path.join(self._working_path,"images",str(self.job.id),"annotation_fix"),str(id_term))
+            list_moving = os.listdir(os.path.join(self._working_path,"images",str(self.job.id),"annotation_moving"),str(id_term))
 
-            # read images #
-            fix_image = sitk.ReadImage(self._working_path + "image_fix" + "/" + str(id_term) + "/" + list_fix[0],   sitk.sitkFloat32)
-            mov_image = sitk.ReadImage(self._working_path + "image_mov" + "/" + str(id_term) + "/" + list_moving[0], sitk.sitkFloat32)
+            # format paths #
+            path_to_fix_image = os.path.join(self._working_path,"images",str(self.job.id),"annotation_fix",str(id_term),str(list_fix[0]))
+            path_to_moving_image = os.path.join(self._working_path,"images",str(self.job.id),"annotation_moving",str(id_term),str(list_moving[0]))
 
-            # instanciate and processing with SimpleElastix #
-            simple_elastix = sitk.SimpleElastix()
-            simple_elastix.SetFixedImage(fix_image)
-            simple_elastix.SetMovingImage(mov_image)
-            parameterMapTranslation = sitk.GetDefaultParameterMap("translation")
-            parameterMapAffine = sitk.GetDefaultParameterMap("affine")
+            # debug #
+            print "path_to_fix_image : " + str(path_to_fix_image)
+            print "path_to_moving_image" + str(path_to_moving_image)
 
-            # 2 params, Translation and affine #
-            simple_elastix.SetParameterMap(parameterMapTranslation)
-            simple_elastix.AddParameterMap(parameterMapAffine)
 
-            # params set by user #
-            simple_elastix.SetParameter("MaximumNumberOfIterations", str(self._nb_iterations))
-            simple_elastix.SetParameter("NumberOfSpatialSamples", str(self._nb_spatialsampels))
 
-            # run processing #
-            simple_elastix.Execute()
+        # load images #
+        fix_image = sitk.ReadImage(path_to_fix_image, sitk.sitkFloat32)
+        moving_image = sitk.ReadImage(path_to_moving_image, sitk.sitkFloat32)
 
-            # get result image and fix image for upload #
-            img = simple_elastix.GetResultImage()
-            np_img = sitk.GetArrayFromImage(img)
-            img_to_save = np.zeros((np_img.shape[0], np_img.shape[1], 3))
-            img_to_save[:, :, 1] = np_img
-            img_to_save[:, :, 2] = sitk.GetArrayFromImage(fix_image)
-            misc.imsave(self._working_path + "/images/" + "result_translationaffine.png", img_to_save)
+        # start processing algorithm #
+        simple_elastix = sitk.SimpleElastix()
+        simple_elastix.SetFixedImage(fix_image)
+        simple_elastix.SetMovingImage(moving_image)
+        parameterMapTranslation = sitk.GetDefaultParameterMap("translation")
+        parameterMapAffine = sitk.GetDefaultParameterMap("affine")
 
-            # upload config #
-            uploadConn = Cytomine(self._cytomine_upload, self._pk, self._prk, verbose=False)
-            sync = False
+        # translation & affine #
+        simple_elastix.SetParameterMap(parameterMapTranslation)
+        simple_elastix.AddParameterMap(parameterMapAffine)
 
-            # uploading image #
-            uploadConn.upload_image(self._working_path + "/images/" + "result_translationaffine.png",
-                                    self._project_id, self._storage_id, "http://" + self._cytomine_host)
+        # params set by user #
+        simple_elastix.SetParameter("MaximumNumberOfIterations", str(self._nb_iterations))
+        simple_elastix.SetParameter("NumberOfSpatialSamples", str(self._nb_spatial_sampel))
+
+        # start computing #
+        simple_elastix.Execute()
+
+        # format image result #
+        img = simple_elastix.GetResultImage()
+        np_img = sitk.GetArrayFromImage(img)
+        img_to_save = np.zeros((np_img.shape[0], np_img.shape[1], 3))
+        img_to_save[:, :, 1] = np_img
+        img_to_save[:, :, 2] = sitk.GetArrayFromImage(fix_image)
+        img_to_save_path = os.path.join(self._working_path,"images",str(self._storage_id),"result_translationaffine.png")
+        misc.imsave(img_to_save_path, img_to_save)
+
+        # connection to demo-upload #
+        demo_upload = Cytomine(self._cytomine_upload, self._pk, self._prk, verbose=True)
+
+        demo_upload.upload_image(img_to_save_path,self._project_id, self._storage_id, "http://demo.cytomine.be")
 
 def main(argv):
 
@@ -169,7 +149,8 @@ def main(argv):
     parser.add_argument('--nb_iterations', dest="nb_iterations", type=long)
     parser.add_argument("--nb_spatialsampels", dest="nb_spatialsampels", type=long)
     parser.add_argument("--cytomine_storage_id",dest="storage_id",type=long)
-    parser.add_argument("--cytomine_id_annotation", dest="id_annotation",type=long)
+    parser.add_argument("--cytomine_id_annotation_fix", dest="id_annotation_fix",type=long)
+    parser.add_argument("--cytomine_id_annotation_moving", dest="id_annotation_moving", type=long)
     parser.add_argument("--cytomine_working_path",dest="working_path")
     parser.add_argument("--cytomine_upload",dest ="cytomine_upload")
 
@@ -189,8 +170,8 @@ def main(argv):
                  (
                     cytomine, arguments.cytomine_id_software, arguments.cytomine_id_project, arguments.__dict__,
                     arguments.id_fix_image, arguments.id_mov_image, arguments.nb_iterations, arguments.nb_spatialsampels,
-                    arguments.storage_id, arguments.id_annotation, arguments.working_path, arguments.cytomine_upload,
-                    arguments.cytomine_public_key, arguments.cytomine_private_key
+                    arguments.storage_id, arguments.id_annotation_fix, arguments.id_annotation_moving, arguments.working_path,
+                    arguments.cytomine_upload,arguments.cytomine_public_key, arguments.cytomine_private_key
                  ) as context:
 
         context.run()
