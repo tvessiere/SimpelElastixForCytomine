a9e2f46d334b60a78b77297a5bc997d7b38d9a08
tvessiere <vessiere.thomas@hotmail.com>
2017-04-28 15:34:13 +0200

    all done, need to test it.

--- add_software_local.py
+++ add_software_local.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     )
 
     #add software with execute command
-    software = conn.add_software("SimpleElastixV25","createRabbitJobWithArgsService","download","/root/miniconda2/envs/cytomine/bin/python /software_router/algo/simple_elastix/get_and_move.py " +
+    software = conn.add_software("SimpleElastixV27","createRabbitJobWithArgsService","download","/root/miniconda2/envs/cytomine/bin/python /software_router/algo/simple_elastix/get_and_move.py " +
                                                                                                      "--cytomine_host $host " +
                                                                                                      "--cytomine_public_key $publicKey " +
                                                                                                      "--cytomine_private_key $privateKey " +
@@ -47,16 +47,17 @@ if __name__ == "__main__":
                                  )
 
 
-    conn.add_software_parameter(name="cytomine_id_software",id_software=software.id,type= "Number",default_value= 0,required= True, index=100, set_by_server=True)
-    conn.add_software_parameter(name="cytomine_id_project",id_software=software.id,type= "Number",default_value= 0,required= True, index=110, set_by_server=True)
+    conn.add_software_parameter(name="cytomine_id_software",id_software=software.id,type= "Number",default_value= 0,required= True, index=120, set_by_server=True)
+    conn.add_software_parameter(name="cytomine_id_project",id_software=software.id,type= "Number",default_value= 0,required= True, index=130, set_by_server=True)
 
     conn.add_software_parameter(name="fix_image_id", type="Domain", id_software=software.id, default_value=None,required=False,index=10,set_by_server=False,uri="/api/project/$currentProject$/imageinstance.json",uriPrintAttribut="instanceFilename",uriSortAttribut="instanceFilename")
     conn.add_software_parameter(name="mov_image_id",type="Domain", id_software=software.id, default_value=None,required=False,index=20,set_by_server=False,uri="/api/project/$currentProject$/imageinstance.json",uriPrintAttribut="instanceFilename",uriSortAttribut="instanceFilename")
-    conn.add_software_parameter(name="nb_iterations",id_software=software.id,type= "Number",default_value=512,required=True,index=30,set_by_server=False)
-    conn.add_software_parameter(name="nb_spatial_sample",id_software=software.id,type= "Number",default_value=2048,required=True,index=40,set_by_server=False)
-    conn.add_software_parameter(name="storage",type="Domain", id_software=software.id, default_value=None,required=False,index=50,set_by_server=False,uri="/api/storage.json",uriPrintAttribut="name",uriSortAttribut="name")
-    conn.add_software_parameter(name="annotation_fix_id",id_software=software.id,type= "Number",default_value=49526,required=False,index=60,set_by_server=False)
-    conn.add_software_parameter(name="annotation_moving_id",id_software=software.id,type= "Number",default_value=49888,required=False,index=70,set_by_server=False)
-    conn.add_software_parameter(name="export_overlay_images" , id_software= software.id, type="Boolean", default_value=False,required=True, index= 120, set_by_server=False)
-    conn.add_software_parameter(name="result_file_name", id_software=software.id, type="String",default_value="result_Simple_Elastix.png",required=True, index=130, set_by_server=False)
-    conn.add_software_parameter(name="number_of_resolution", id_software=software.id, type="Number", default_value=4, required=True, index=140, set_by_server=False)
+    conn.add_software_parameter(name="nb_iterations",id_software=software.id,type= "Number",default_value=512,required=True,index=50,set_by_server=False)
+    conn.add_software_parameter(name="nb_spatial_sample",id_software=software.id,type= "Number",default_value=2048,required=True,index=60,set_by_server=False)
+    conn.add_software_parameter(name="storage",type="Domain", id_software=software.id, default_value=None,required=False,index=90,set_by_server=False,uri="/api/storage.json",uriPrintAttribut="name",uriSortAttribut="name")
+    conn.add_software_parameter(name="annotation_fix_id",id_software=software.id,type= "Number",default_value=49526,required=False,index=30,set_by_server=False)
+    conn.add_software_parameter(name="annotation_moving_id",id_software=software.id,type= "Number",default_value=49888,required=False,index=40,set_by_server=False)
+    conn.add_software_parameter(name="export_overlay_images" , id_software= software.id, type="Boolean", default_value=False,required=True, index= 100, set_by_server=False)
+    conn.add_software_parameter(name="result_file_name", id_software=software.id, type="String",default_value="result_Simple_Elastix.png",required=True, index=80, set_by_server=False)
+    conn.add_software_parameter(name="number_of_resolution", id_software=software.id, type="Number", default_value=4, required=True, index=70, set_by_server=False)
+    conn.add_software_parameter(name="cytomine_upload", id_software=software.id, type="String", default_value="demo-upload.cytomine.be",required=True, index=110, set_by_server=False)
