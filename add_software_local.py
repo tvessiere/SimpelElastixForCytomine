2334a4b85f7c5c8390eedd7ba8cca419d65b7ca8
tvessiere <vessiere.thomas@hotmail.com>
2017-04-28 14:43:37 +0200

    all done, need to test it.

--- add_software_local.py
+++ add_software_local.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     )
 
     #add software with execute command
-    software = conn.add_software("SimpleElastixV19","createRabbitJobWithArgsService","download","/root/miniconda2/envs/cytomine/bin/python /software_router/algo/simple_elastix/get_and_move.py "+
+    software = conn.add_software("SimpleElastixV25","createRabbitJobWithArgsService","download","/root/miniconda2/envs/cytomine/bin/python /software_router/algo/simple_elastix/get_and_move.py " +
                                                                                                      "--cytomine_host $host " +
                                                                                                      "--cytomine_public_key $publicKey " +
                                                                                                      "--cytomine_private_key $privateKey " +
@@ -41,7 +41,7 @@ if __name__ == "__main__":
                                                                                                      "--annotation_fix_id $annotation_fix_id " +
                                                                                                      "--annotation_moving_id $annotation_moving_id " +
                                                                                                      "--cytomine_upload $cytomine_upload " +
-                                                                                                     "--export_overlay_images $export_overlay_images "
+                                                                                                     "--export_overlay_images $export_overlay_images " +
                                                                                                      "--result_file_name $result_file_name " +
                                                                                                      "--number_of_resolution $number_of_resolution "
                                  )
@@ -55,8 +55,8 @@ if __name__ == "__main__":
     conn.add_software_parameter(name="nb_iterations",id_software=software.id,type= "Number",default_value=512,required=True,index=30,set_by_server=False)
     conn.add_software_parameter(name="nb_spatial_sample",id_software=software.id,type= "Number",default_value=2048,required=True,index=40,set_by_server=False)
     conn.add_software_parameter(name="storage",type="Domain", id_software=software.id, default_value=None,required=False,index=50,set_by_server=False,uri="/api/storage.json",uriPrintAttribut="name",uriSortAttribut="name")
-    conn.add_software_parameter(name="annotation_fix_id",id_software=software.id,type= "Number",default_value=None,required=False,index=60,set_by_server=False)
-    conn.add_software_parameter(name="annotation_moving_id",id_software=software.id,type= "Number",default_value=None,required=False,index=70,set_by_server=False)
+    conn.add_software_parameter(name="annotation_fix_id",id_software=software.id,type= "Number",default_value=49526,required=False,index=60,set_by_server=False)
+    conn.add_software_parameter(name="annotation_moving_id",id_software=software.id,type= "Number",default_value=49888,required=False,index=70,set_by_server=False)
     conn.add_software_parameter(name="export_overlay_images" , id_software= software.id, type="Boolean", default_value=False,required=True, index= 120, set_by_server=False)
     conn.add_software_parameter(name="result_file_name", id_software=software.id, type="String",default_value="result_Simple_Elastix.png",required=True, index=130, set_by_server=False)
     conn.add_software_parameter(name="number_of_resolution", id_software=software.id, type="Number", default_value=4, required=True, index=140, set_by_server=False)
